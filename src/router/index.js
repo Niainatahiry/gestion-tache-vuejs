f878d76f642659f35a002b5c56494b1e9e2f3fb6
Niainatahiry <Rakottahiry@gmail.com>
2024-02-26 13:28:23 +0300

    ajout d'une condition meta pour que les pages liées au tâches requirent une authentification

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import InscriptionView from '../views/InscriptionView.vue'
-import ConnexionView from '../views/ConnexionView.vue'
-import TaskListView from '@/views/tasks/TaskListView.vue'
-import CreateTaskView from '@/views/tasks/CreateTaskView.vue'
-import EditTaskView from '@/views/tasks/EditTaskView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import InscriptionView from '../views/InscriptionView.vue';
+import ConnexionView from '../views/ConnexionView.vue';
+import TaskListView from '@/views/tasks/TaskListView.vue';
+import CreateTaskView from '@/views/tasks/CreateTaskView.vue';
+import EditTaskView from '@/views/tasks/EditTaskView.vue';
+import store from '@/stores/connexion'; // Assurez-vous d'importer votre store Vuex
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
     {
       path: '/',
       name: 'inscription',
@@ -22,19 +22,40 @@ const router = createRouter({
     {
       path: '/taskList',
       name: 'taskList',
-      component: TaskListView
+      component: TaskListView,
+      meta: { requiresAuth: true } // Indique que cette route nécessite une authentification
     },
     {
       path: '/creerTache',
       name: 'creerTache',
-      component: CreateTaskView
+      component: CreateTaskView,
+      meta: { requiresAuth: true } // Indique que cette route nécessite une authentification
     },
     {
       path: '/editTask',
       name: 'editTask',
-      component: EditTaskView
+      component: EditTaskView,
+      meta: { requiresAuth: true } // Indique que cette route nécessite une authentification
     }
   ]
-})
+});
 
-export default router
+// Gardien de route pour vérifier l'authentification
+router.beforeEach((to, from, next) => {
+  // Vérifiez si la route nécessite une authentification
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // Si l'utilisateur n'est pas authentifié, redirigez-le vers la page de connexion
+    if (!store.state.auth.isLoggedIn) {
+      next({
+        name: 'connexion',
+        // Vous pouvez passer des paramètres supplémentaires si nécessaire
+      });
+    } else {
+      next(); // Si l'utilisateur est authentifié, continuez la navigation
+    }
+  } else {
+    next(); // Si la route n'a pas besoin d'authentification, continuez la navigation
+  }
+});
+
+export default router;
